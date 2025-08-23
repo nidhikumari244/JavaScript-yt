b7407449076f71387bb8e3714bb4c4e2be676f4d
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-23 15:11:13 +0000

    updated-conversion

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -29,3 +29,50 @@ let someNumber =42
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+
+// *************Operation********** //
+
+
+let value = 3
+let negValue = -value     //console.log(negValue);
+
+// console.log(negValue);
+
+// console.log( 2+2 );
+// console.log( 4-2 );
+// console.log( 6*3 );
+// console.log( 8/2 );
+// console.log( 3%2 );
+// console.log( 2**3 );
+ 
+let str1 = "NIDHI"
+let str2 = " Kumari"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log( "1"+ 2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( 3+2-4*3/3);   //Not correct use clean and readable code
+// //  pls don't use messy codes 
+
+// console.log( (3+2)-2*4/3);  
+
+console.log(+true);
+
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 4
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+

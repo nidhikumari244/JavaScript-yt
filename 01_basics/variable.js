cff91106a79015ebe31ce8fa7b1de469398215d3
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-23 06:45:27 +0000

    variable

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const accountId=123456
+let accountEmail="nidhigoogle.com"
+var accountPassword="nidhi46"
+accountCity="Noida"
+let accountState;
+// accountId=2   // not allowed 
+
+accontEmail="ndh@google";
+accountPassword="24622462"
+accountCity="Bengaluru"
+console.log(accountId);
+
+/** Prefer not to use var
+ * because of issue in block scope and functional scope
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

dfce893580f7f4c1eff9bf78895c18f7c5fd8a4e
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-23 06:54:29 +0000

    added variable.js

---
+++ 01_basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log(3+3);
+// code readibility must be good  
+console.log("nidhi")
+
+let name="NIDHI";
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 54
+
+// brigint
+//srting=> ""
+// boolean => true/false
+// nun => standalone value
+//undefined=> 
+    // symbol => unique
+// object 
+
+console.log(typeof age);

7a6d8871228d0ebdc45a9ae2daeb0352498e3ee0
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-24 14:14:48 +0000

    comparision

---
+++ 01_basics/comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<3);
+// console.log(2==1);
+// console.log(5!=2);
+ 
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+         // avoid this type of comparision always use clean and clear code...:)
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined>=0);
+
+// ====
+
+console.log("2"===2);

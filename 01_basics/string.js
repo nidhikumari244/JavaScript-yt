0396d76d7623591867ac81b327554303b8290016
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-25 16:55:20 +0000

    Strings

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -6,6 +6,32 @@ const repoCount = 9.8
 
 console.log(`HEllo my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('NIDHI')
+const gameName = new String('NIDHI-google-engineer')
 
 console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('D'));
+
+
+const newString =gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-2,3)
+console.log(anotherString);
+
+const newStringOne="   nidhi  "
+console.log(newStringOne);
+ console.log(newStringOne.trim());
+ const url = "https://nidhigoogle.com/nidhi%19kumari"
+ console.log(url.replace('%19','_'));
+ 
+ console.log(url.includes('Krishna'))
+
+ console.log(gameName.split('---'));
+

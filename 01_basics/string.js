b3e709c2df37e9f2b7cefff3439c8330fc089a31
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-25 14:36:59 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,11 @@
+const name = "NiDhi "
+const repoCount = 9.8
+
+// console.log(name + repoCount + " CGPA");
+
+
+console.log(`HEllo my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('NIDHI')
+
+console.log(gameName[0]);

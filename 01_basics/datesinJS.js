e44bb0b28a92e3e0af313ea58dc4c8b9d241885c
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-26 17:02:49 +0000

    DatesInJS

---
+++ 01_basics/datesinJS.js
@@ -0,0 +1,35 @@
+// Dates 
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toDateString);
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2025,0,25)
+// let myCreatedDate = new Date(2025,0,25,6,4,3)
+// let myCreatedDate = new Date("2005-04-24")
+let myCreatedDate = new Date("2005-04-24")
+//console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+//`${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+
+})

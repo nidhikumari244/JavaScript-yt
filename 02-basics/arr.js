15a9f2fa64df07caf3d35a9cd014e61cc01711fd
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-27 16:41:23 +0000

    array part 2

--- 02-basics/arr.js
+++ 02-basics/arr.js
@@ -1,1 +1,32 @@
 // A R R A Y part 2 //
+
+const marvel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// marvel_heros.concat(dc_heros)
+// console.log(allheros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+console.log(all_new_heros);
+
+const another_array = [1,2,3,4,5,6,7,[12,3,5],[14,6[4,5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array);
+
+
+console.log(Array.isArray("NIDHI"));
+console.log(Array.from("NIDHI"));
+console.log(Array.from({name: "NIDHI"}))    //intersting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));

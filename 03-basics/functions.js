8338877a420585a99dfbc6c2163a4bd573f7ecf6
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-08-30 16:29:16 +0000

    functions

--- 03-basics/functions.js
+++ 03-basics/functions.js
@@ -36,10 +36,44 @@ function loginUserMessage(username="ndh")
 {
    if(username === undefined)
     {
-console.log("pls enter the user nmae");
+//console.log("pls enter the user nmae");
 return 
    }
     return `${username} just logged in`
 }
 //console.log(loginUserMessage("nidhi"));
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
+
+
+
+
+function calculateCartPrice(val1,val2,...num1){
+return num1
+}
+
+//console.log(calculateCartPrice(200,400,500))
+const user ={
+    username: "nidhi",
+    price: 199
+
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is $
+        {anyobject.price}`);
+}
+
+handleObject(user)
+handleObject({
+    username: "ndh",
+    price: 500
+})
+
+const myNewArray = [200,400,600,800]
+
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+//console.log(returnSecondValue([200,400,600,1000]));

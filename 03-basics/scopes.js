6e99dc1988f93e56b799ee5ca9399228bb368e54
Nidhi Kumari <nidhigupta2462@gmail.com>
2025-09-02 19:31:48 +0000

    scope

--- 03-basics/scopes.js
+++ 03-basics/scopes.js
@@ -2,7 +2,7 @@ let c = 400
  if (true) {
    const b = 10
     let c = 30
-    console.log("inner:" ,c);
+    //console.log("inner:" ,c);
     
 }
     
@@ -10,6 +10,21 @@ let c = 400
 
 //console.log(a);
 //console.log(b);
-console.log(c);
+//onsole.log(c);
 
 
+function one(){
+    const username ="nidhi"
+    function two(){
+        const website ="yt"
+        console.log(username);
+        
+    }
+
+//console.log(website);
+
+two()  // if we comment this line it will not execute
+
+}
+
+one()
